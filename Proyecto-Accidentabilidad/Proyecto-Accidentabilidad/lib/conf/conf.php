0288b7e3690627c1fdc3ccdb4683e86037cbbefa
SebastianGG <parajugarclashenlapc@gmail.com>
2026-06-03 21:43:16 -0500

    Actualizacion del proyecto

--- Proyecto-Accidentabilidad/Proyecto-Accidentabilidad/lib/conf/conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $server= "localhost";
-    $user = "postgres";
-    $pass = "sena";
-    $db = "GIAV";
-    $port = 5432;
-    //postgres -> 5432
-?>
--- Proyecto-Accidentabilidad/Proyecto-Accidentabilidad/web/ajax.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include_once '../lib/helpers.php';
-    if(isset($_GET['modulo'])){
-        resolver();
-    }
-?>
