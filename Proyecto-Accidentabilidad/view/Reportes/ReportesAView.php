ae81da16819a995dee4f64d62517cc0a571adc27
SebastianGG <parajugarclashenlapc@gmail.com>
2026-06-05 01:46:59 -0500

    Actualizacion de reportesController, script y reportesAView

--- Proyecto-Accidentabilidad/view/Reportes/ReportesAView.php
+++ Proyecto-Accidentabilidad/view/Reportes/ReportesAView.php
@@ -2,49 +2,47 @@
     <h3 class="display-3">Formulario reportes accidentes</h3>
 </div>
 
-<form action="<?php echo getUrl("Reportes","Reportes","postCreate");?>" 
+<form action="<?php echo getUrl("Reportes","ReportesA","postCreate");?>" 
       method="post" enctype="multipart/form-data">
 
     <div class="row mt-5">
 
         <div class="col-md-4">
-            <label for="fechaaccidente">fecha del accidente</label>
-            <input type="text" class="form-control" id="usuario" name="fechaaccidente" placeholder="fecha del accidente">
+            <label for="fechaaccidente">Fecha del accidente</label>
+            <input type="date" class="form-control" id="fechaaccidente" name="fechaaccidente" required>
         </div>
 
         <div class="col-md-4">
-            <label for="nomenclaura">Nomenclatura</label>
-            <input type="text" class="form-control" id="usuario" name="nomenclatura" placeholder="nomenclatura">
+            <label for="nomenclatura">Nomenclatura</label>
+            <input type="text" class="form-control" id="nomenclatura" name="nomenclatura" placeholder="nomenclatura" required>
         </div>
 
         <div class="col-md-4">
-            <label for="nlesionados">Numero</label>
-            <input type="number" class="form-control" id="usuario" name="nlesionados" placeholder="numero de lesionados">
+            <label for="nlesionados">Número de lesionados</label>
+            <input type="number" class="form-control" id="nlesionados" name="nlesionados" placeholder="número de lesionados" min="0" required>
         </div>
 
         <div class="col-md-4">
-            <label for="observaciones">Obervaciones </label>
-            <textarea id="usuario" name="observaciones" rows="3" cols="40" placeholder="observaciones"> </textarea>
+            <label for="observaciones">Observaciones</label>
+            <textarea class="form-control" id="observaciones" name="observaciones" rows="3" placeholder="observaciones"></textarea>
         </div>
 
         <div class="col-md-4">
-            <div class="col-md-4">
-            <label class="form-label"> Inserta una imagen</label>
-            <input type="file" class="form-control" id="imagen" name="imagen" >
-        </div>
+            <label class="form-label" for="imagen">Inserta una imagen</label>
+            <input type="file" class="form-control" id="imagen" name="imagen" accept="image/*">
         </div>
         
-         <div class="col-md-4">
-            <label for="direccion">Direccion</label>
-            <input type="text" class="form-control" id="usuario" name="direccion" placeholder="direccion">
+        <div class="col-md-4">
+            <label for="direccion">Dirección</label>
+            <input type="text" class="form-control" id="direccion" name="direccion" placeholder="dirección" required>
         </div>
 
         <div class="col-md-4">
-            <label for="tchoque">Choque</label>
-            <select name="tchoque" id="tchoque" class="form-control">
+            <label for="tchoque">Tipo de Choque</label>
+            <select name="tchoque" id="tchoque" class="form-control" required>
                 <?php
                     while ($choque = pg_fetch_assoc($reportes)) {
-                        echo "<option value='" . $choque['id_tipo_choque'] . "'>" . $choque['nombre_choque'] . "</option>";
+                        echo "<option value='" . $choque['id_tipo_choque'] . "'>" . $choque['nombre'] . "</option>";
                     }
                 ?>
             </select>
@@ -57,3 +55,5 @@
     </div>
 
 </form>
+
+<?php include_once "../view/partials/script.php"; ?>
