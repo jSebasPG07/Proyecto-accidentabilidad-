8c8e6acca4193c44ca86c3f9fff9263ddd4d9dc2
gabriel <chanagabriel43@gmail.com>
2026-06-05 09:57:14 -0500

    MVC De Solicitud Via en Mal Estado

---
+++ Proyecto-Accidentabilidad/controller/Reportes/SolicitudVMEController.php
@@ -0,0 +1,48 @@
+<?php 
+
+include_once "../model/Reportes/SolicitudVMEModel.php";
+
+class SolicitudVMEController {
+
+    public function getCreate(){
+        $obj = new SolicitudVMEModel();
+        $sql = "SELECT * FROM tipo_dano_via";
+        $tiposDanoVia = $obj->select($sql);
+        include_once "../view/Reportes/SolicitudVMEView.php"
+    }
+
+    public function postCreate(){
+        $obj = new SolicitudVMEModel();
+
+        $descripcion = $_POST['descripcion'];
+        $direccion = $_POST['direccion'];
+        $idtipodanovia = $_POST['idtipodanovia'];
+
+        $id_estado = 3;
+
+        $img = $_FILES['imagen']['name'];
+        $archivo = $_FILES['imagen']['tmp_name'];
+        $ruta = "../img" . $img;
+
+        if (move_uploaded_file($archivo, $ruta)) {
+            
+            $sql = "INSERT INTO sol_via_mal_estado (descripcion, imagen_url, direccion, id_estado, id_tipo_dano_via) 
+                    VALUES ('$descripcion', '$ruta', '$direccion', '$id_estado', '$idtipodanovia')";
+
+            $ejecutar = $obj->insert($sql);
+
+            if($ejecutar) {
+                echo "<script>window.location.href='" . getUrl("Reportes", "SolicitudVME", "getCreate") . "&msg=ok';</script>";
+            } else {
+                echo "<script>window.location.href='" . getUrl("Reportes", "SolicitudVME", "getCreate") . "&msg=error';</script>";
+            }
+        } else {
+            echo "<script>window.location.href='" . getUrl("Reportes", "SolicitudVME", "getCreate") . "&msg=imgerror';</script>";
+        }
+
+    }
+    
+}
+
+
+?>
---
+++ Proyecto-Accidentabilidad/view/Reportes/SolicitudVMEView.php
@@ -0,0 +1,58 @@
+<div class="mt-3">
+    <h3 class="display-3">Solicitud vía en mal estado</h3>
+</div>
+ 
+<form action="<?php echo getUrl('Reportes', 'SolicitudVME', 'postCreate'); ?>"
+      method="post" enctype="multipart/form-data">
+ 
+    <div class="row mt-5">
+ 
+        <div class="col-md-4">
+            <label for="idtipodanovia">Tipo de daño</label>
+            <select name="idtipodanovia" id="idtipodanovia" class="form-control">
+                <?php while ($dano = pg_fetch_assoc($tiposDanoVia)) { ?>
+                    <option value="<?php echo $dano['id_tipo_dano_via']; ?>">
+                        <?php echo $dano['descripcion']; ?>
+                    </option>
+                <?php } ?>
+            </select>
+        </div>
+ 
+        <div class="col-md-4">
+            <label for="direccion">Dirección</label>
+            <input type="text" class="form-control" id="direccion" name="direccion"
+                   placeholder="Ej: Cra 5 # 12-30">
+        </div>
+ 
+        <div class="col-md-4">
+            <label for="imagen">Imagen</label>
+            <input type="file" class="form-control" id="imagen" name="imagen">
+        </div>
+ 
+        <div class="col-md-4 mt-3">
+            <label for="descripcion">Descripción</label>
+            <textarea class="form-control" id="descripcion" name="descripcion"
+                      rows="3" placeholder="Describa el estado de la vía"></textarea>
+        </div>
+ 
+        <div class="col-md-12 mt-4">
+            <button type="submit" class="btn btn-success">Enviar solicitud</button>
+        </div>
+ 
+    </div>
+ 
+</form>
+ 
+<script>
+    $('form').on('submit', function(e) {
+        var descripcion = $('#descripcion').val().trim();
+        var direccion   = $('#direccion').val().trim();
+        var imagen      = $('#imagen').val();
+ 
+        if (descripcion === '' || direccion === '' || imagen === '') {
+            e.preventDefault();
+            swal("Campos incompletos", "Por favor llena todos los campos.", "warning");
+        }
+    });
+</script>
+ 
