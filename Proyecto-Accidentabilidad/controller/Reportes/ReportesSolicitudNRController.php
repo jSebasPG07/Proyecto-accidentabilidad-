af6b0a49c830dfa5559b39740417fa7461666694
Jan carlos ararat <jararatcordoba@gmail.com>
2026-06-11 12:43:46 -0500

    Resolver conflictos de ReportesSolicitudNR

--- Proyecto-Accidentabilidad/controller/Reportes/ReportesSolicitudNRController.php
+++ Proyecto-Accidentabilidad/controller/Reportes/ReportesSolicitudNRController.php
@@ -5,10 +5,6 @@ include_once "../model/Reportes/ReportesSolicitudNRModel.php";
 class ReportesSolicitudNRController {
 
     public function index(){
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
         $this->getCreate();
     }
 
@@ -16,15 +12,12 @@ class ReportesSolicitudNRController {
 
         $obj = new ReportesSolicitudNRModel();
 
-<<<<<<< Updated upstream
         $sqlReductor = "SELECT * FROM tipo_reductor";
         $reductores = $obj->select($sqlReductor);
 
         $sqlDano = "SELECT * FROM tipo_dano_reductor";
         $danos = $obj->select($sqlDano);
 
-=======
->>>>>>> Stashed changes
         include_once "../view/Reportes/ReportesSolicitudNR.php";
     }
 
@@ -32,7 +25,6 @@ class ReportesSolicitudNRController {
 
         $obj = new ReportesSolicitudNRModel();
 
-<<<<<<< Updated upstream
         $descripcion = $_POST['descripcion'];
         $direccion = $_POST['direccion'];
         $idTipoReductor = $_POST['id_tipo_reductor'];
@@ -41,23 +33,11 @@ class ReportesSolicitudNRController {
 
         $idEstado = 3;
         $id_usuario = $_POST['id'];
-=======
-        $tipoDano = $_POST['tipo_dano'];
-        $descripcion = $_POST['descripcion'];
-        $direccion = $_POST['direccion'];
-        $idTipoReductor = $_POST['id_tipo_reductor'];
-
-        $idEstado = 1;
-
-        $nombreImagen = "";
-        $archivo = $_FILES['imagen_url']['tmp_name'];
->>>>>>> Stashed changes
 
         if(isset($_FILES['imagen_url']) && $_FILES['imagen_url']['error'] == 0){
 
             $nombreImagen = time() . "_" . $_FILES['imagen_url']['name'];
 
-<<<<<<< Updated upstream
             $ruta = "../img/" . $nombreImagen;
 
             if(move_uploaded_file($_FILES['imagen_url']['tmp_name'], $ruta)){
@@ -82,30 +62,6 @@ class ReportesSolicitudNRController {
                     '$idTipoDanoReductor',
                     '$idUsuario'
                 )";
-=======
-            $ruta = "../uploads/reductores/" . $nombreImagen;
-
-            if(move_uploaded_file($archivo, $ruta)){
-
-                $sql = "INSERT INTO sol_nuevo_reductor
-                        (
-                            tipo_dano,
-                            descripcion,
-                            imagen_url,
-                            direccion,
-                            id_tipo_reductor,
-                            id_estado
-                        )
-                        VALUES
-                        (
-                            '$tipoDano',
-                            '$descripcion',
-                            '$ruta',
-                            '$direccion',
-                            '$idTipoReductor',
-                            '$idEstado'
-                        )";
->>>>>>> Stashed changes
 
                 $ejecutar = $obj->insert($sql);
 
@@ -116,20 +72,9 @@ class ReportesSolicitudNRController {
                 }
 
             }else{
-<<<<<<< Updated upstream
                 echo "<script>window.location.href='".getUrl("Reportes","ReportesSolicitudNR","getCreate")."&msg=imgerror';</script>";
             }
-=======
-
-                echo "<script>window.location.href='".getUrl("Reportes","ReportesSolicitudNR","getCreate")."&msg=imgerror';</script>";
-            }
-
-        }else{
-
-            echo "<script>window.location.href='".getUrl("Reportes","ReportesSolicitudNR","getCreate")."&msg=imgerror';</script>";
->>>>>>> Stashed changes
         }
     }
 }
-
 ?>
