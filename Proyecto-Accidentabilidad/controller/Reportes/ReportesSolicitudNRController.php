3215e8811719bd86b3d628af4b1d46019b40bb47
Jan carlos ararat <jararatcordoba@gmail.com>
2026-06-11 12:24:32 -0500

    Cargue archivos

--- Proyecto-Accidentabilidad/controller/Reportes/ReportesSolicitudNRController.php
+++ Proyecto-Accidentabilidad/controller/Reportes/ReportesSolicitudNRController.php
@@ -5,6 +5,10 @@ include_once "../model/Reportes/ReportesSolicitudNRModel.php";
 class ReportesSolicitudNRController {
 
     public function index(){
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         $this->getCreate();
     }
 
@@ -12,12 +16,15 @@ class ReportesSolicitudNRController {
 
         $obj = new ReportesSolicitudNRModel();
 
+<<<<<<< Updated upstream
         $sqlReductor = "SELECT * FROM tipo_reductor";
         $reductores = $obj->select($sqlReductor);
 
         $sqlDano = "SELECT * FROM tipo_dano_reductor";
         $danos = $obj->select($sqlDano);
 
+=======
+>>>>>>> Stashed changes
         include_once "../view/Reportes/ReportesSolicitudNR.php";
     }
 
@@ -25,6 +32,7 @@ class ReportesSolicitudNRController {
 
         $obj = new ReportesSolicitudNRModel();
 
+<<<<<<< Updated upstream
         $descripcion = $_POST['descripcion'];
         $direccion = $_POST['direccion'];
         $idTipoReductor = $_POST['id_tipo_reductor'];
@@ -33,11 +41,23 @@ class ReportesSolicitudNRController {
 
         $idEstado = 3;
         $id_usuario = $_POST['id'];
+=======
+        $tipoDano = $_POST['tipo_dano'];
+        $descripcion = $_POST['descripcion'];
+        $direccion = $_POST['direccion'];
+        $idTipoReductor = $_POST['id_tipo_reductor'];
+
+        $idEstado = 1;
+
+        $nombreImagen = "";
+        $archivo = $_FILES['imagen_url']['tmp_name'];
+>>>>>>> Stashed changes
 
         if(isset($_FILES['imagen_url']) && $_FILES['imagen_url']['error'] == 0){
 
             $nombreImagen = time() . "_" . $_FILES['imagen_url']['name'];
 
+<<<<<<< Updated upstream
             $ruta = "../img/" . $nombreImagen;
 
             if(move_uploaded_file($_FILES['imagen_url']['tmp_name'], $ruta)){
@@ -62,6 +82,30 @@ class ReportesSolicitudNRController {
                     '$idTipoDanoReductor',
                     '$idUsuario'
                 )";
+=======
+            $ruta = "../uploads/reductores/" . $nombreImagen;
+
+            if(move_uploaded_file($archivo, $ruta)){
+
+                $sql = "INSERT INTO sol_nuevo_reductor
+                        (
+                            tipo_dano,
+                            descripcion,
+                            imagen_url,
+                            direccion,
+                            id_tipo_reductor,
+                            id_estado
+                        )
+                        VALUES
+                        (
+                            '$tipoDano',
+                            '$descripcion',
+                            '$ruta',
+                            '$direccion',
+                            '$idTipoReductor',
+                            '$idEstado'
+                        )";
+>>>>>>> Stashed changes
 
                 $ejecutar = $obj->insert($sql);
 
@@ -72,9 +116,20 @@ class ReportesSolicitudNRController {
                 }
 
             }else{
+<<<<<<< Updated upstream
                 echo "<script>window.location.href='".getUrl("Reportes","ReportesSolicitudNR","getCreate")."&msg=imgerror';</script>";
             }
+=======
+
+                echo "<script>window.location.href='".getUrl("Reportes","ReportesSolicitudNR","getCreate")."&msg=imgerror';</script>";
+            }
+
+        }else{
+
+            echo "<script>window.location.href='".getUrl("Reportes","ReportesSolicitudNR","getCreate")."&msg=imgerror';</script>";
+>>>>>>> Stashed changes
         }
     }
 }
+
 ?>
--- Proyecto-Accidentabilidad/view/Reportes/ReportesSolicitudNR.php
+++ Proyecto-Accidentabilidad/view/Reportes/ReportesSolicitudNR.php
@@ -10,6 +10,7 @@
 
         <div class="col-md-4">
             <label for="id_tipo_reductor">Tipo de Reductor</label>
+<<<<<<< Updated upstream
             <select name="id_tipo_reductor"
                     id="id_tipo_reductor"
                     class="form-control"
@@ -90,3 +91,64 @@
 </form>
 
 <?php include_once "../view/partials/script.php"; ?>
+=======
+            <select name="id_tipo_reductor" id="id_tipo_reductor" class="form-control" required>
+                <option value="">Seleccione...</option>
+                <option value="1">Resalto Parabólico</option>
+                <option value="2">Banda Sonora</option>
+                <option value="3">Stoper / Tachón</option>
+            </select>
+        </div>
+
+        <div class="col-md-4">
+            <label for="tipo_dano">Clasificación / Daño</label>
+            <input type="text"
+                   class="form-control"
+                   id="tipo_dano"
+                   name="tipo_dano"
+                   placeholder="Ej: Falta reductor vial"
+                   required>
+        </div>
+
+        <div class="col-md-4">
+            <label for="direccion">Dirección</label>
+            <input type="text"
+                   class="form-control"
+                   id="direccion"
+                   name="direccion"
+                   placeholder="Dirección"
+                   required>
+        </div>
+
+        <div class="col-md-4">
+            <label for="descripcion">Descripción</label>
+            <textarea class="form-control"
+                      id="descripcion"
+                      name="descripcion"
+                      rows="3"
+                      required></textarea>
+        </div>
+
+        <div class="col-md-4">
+            <label for="imagen_url">Fotografía de Evidencia</label>
+            <input type="file"
+                   class="form-control"
+                   id="imagen_url"
+                   name="imagen_url"
+                   accept="image/*"
+                   required>
+        </div>
+
+        <div class="col-md-4">
+            <button type="submit"
+                    class="btn btn-success mt-4">
+                Enviar Solicitud
+            </button>
+        </div>
+
+    </div>
+
+</form>
+
+<?php include_once "../view/partials/script.php"; ?>
+>>>>>>> Stashed changes
